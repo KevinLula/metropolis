e4f53b3bb9edd5d77986a7a3e2fe7aa63d9b30e2
JonW27 <18wongj@gmail.com>
2017-06-01 15:25:25 -0400

    prelim work for station

--- Station.java
+++ Station.java
@@ -1,9 +1,41 @@
-public interface Station{
-        private int size;
-        private People people;
-        private double x, y
+import java.util.Random;
+import java.util.ArrayList;
+public class Station{
+        private int size, x, y, people;
+        private boolean interchange;
+        private ArrayList<Line> lines;
+        private static Random rand = new Random();
 
         public Station(){
+          this(rand.nextInt(100), rand.nextInt(100));
+        }
 
+        public Station(int x, int y){
+          this.x = x;
+          this.y = y;
+          this.size = 1;
+          this.people = 0;
+          this.interchange = false;
+          lines = new ArrayList<Line> lines;
+        }
+        public void addPeople(int people){
+          if(people < 0){
+            throw new IllegalArgumentException();
+          }
+          this.people += people;
+        }
+        public void removePeople(int people){
+          if(this.people - people < 0){
+            throw new IllegalArgumentException();
+          }
+          this.people -= people;
+        }
+        public boolean isInterchange(){
+          return interchange;
+        }
+        public boolean switchInterchange(){
+          // switches interchange state
+          interchange = !interchange;
+          return interchange;
         }
 }
